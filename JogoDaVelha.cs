701265fbc032fd314ae2b7eee9cd50c65a0a6cad
fredmessias43 <fredmessias43@gmail.com>
2020-07-29 19:14:35 -0300

    first commit

---
+++ JogoDaVelha.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JogoDaVelha
+{
+    class JogoDaVelha
+    {   
+        public char[,] matriz = new char[3, 3] {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
+        public int _linha = 777;
+        public int _coluna = 999;
+        public char _dado;
+
+
+        public char[,] Dado(int linha, int coluna, char dado)
+        {
+            this._linha = linha;
+            this._coluna = coluna;
+            this._dado = dado;
+            this.matriz[linha, coluna] = dado;
+            return matriz;
+        }
+
+        public void InsereDadoRandom(char xis)
+        {
+            Random num = new Random();
+            int x;
+            int y;
+            do
+            {
+                x = num.Next(0, 3);
+                y = num.Next(0, 3);
+            } while ((this._linha == x && this._coluna == y) || (this.matriz[x,y]==this._dado));
+
+            this.matriz = Dado(x, y, xis);
+        }
+        /*
+        public bool TesteUserGanhou()
+        {
+        }
+
+        public bool TestePCGanhou()
+        {
+        }
+        */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+
+namespace JogoDaVelha
+{
+    class Program
+    {
+        static JogoDaVelha jogoDaVelha = new JogoDaVelha();
+
+        static void Main(string[] args)
+        {
+            char outraPeca = 'x';
+            char peca = 'o';
+            int ganhou = 1;
+            int cont = 0;
+
+            Console.WriteLine("Esse é o tabuleiro");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{jogoDaVelha.matriz[i, j]}  ");
+                }
+            }
+            Console.WriteLine("\nEscolha se irá jogar com O ou X:");
+            peca = Convert.ToChar(Console.ReadLine());
+
+            if (peca == 'x' || peca == 'X')
+            {
+                outraPeca = 'o';
+            }
+            else
+            {
+                outraPeca = 'x';
+            }
+
+            do
+            {
+                try
+                {
+                    do
+                    {
+                        int linha;
+                        int coluna;
+                        do
+                        {
+                            Console.Write("\nEntre com a linha que fará sua jogada(começa no 1): ");
+                            linha = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Entre com a coluna que fará sua jogada(começa no 1): ");
+                            coluna = Convert.ToInt32(Console.ReadLine());
+                            linha--;
+                            coluna--;
+                        } while (jogoDaVelha._linha == linha && jogoDaVelha._coluna == coluna);
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.WriteLine();
+                            for (int j = 0; j < 3; j++)
+                            {
+                                Console.Write($"{jogoDaVelha.Dado(linha, coluna, peca)[i, j]}  ");
+                            }
+                        }            
+                        
+
+                        //User - HORIZONTAL
+                        if ((jogoDaVelha.matriz[0, 0] == peca) && (jogoDaVelha.matriz[0, 1] == peca) && (jogoDaVelha.matriz[0, 2] == peca))
+                        {
+                            Console.WriteLine("Você ganhou!!!!"); 
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[1, 0] == peca) && (jogoDaVelha.matriz[1, 1] == peca) && (jogoDaVelha.matriz[1, 2] == peca))
+                        {
+                            Console.WriteLine("Você ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[2, 0] == peca) && (jogoDaVelha.matriz[2, 1] == peca) && (jogoDaVelha.matriz[2, 2] == peca))
+                        {
+                            Console.WriteLine("Você ganhou!!!!");
+                            break;
+                        }
+
+                        //User - VERTICAL
+                        if ((jogoDaVelha.matriz[0, 0] == peca) && (jogoDaVelha.matriz[1, 0] == peca) && (jogoDaVelha.matriz[2, 0] == peca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("Você ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[0, 1] == peca) && (jogoDaVelha.matriz[1, 1] == peca) && (jogoDaVelha.matriz[2, 1] == peca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("Você ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[0, 2] == peca) && (jogoDaVelha.matriz[1, 2] == peca) && (jogoDaVelha.matriz[2, 2] == peca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("Você ganhou!!!!");
+                            break;
+                        }
+
+                        //USer - DIAGONAL
+                        if ((jogoDaVelha.matriz[0, 0] == peca) && (jogoDaVelha.matriz[1, 1] == peca) && (jogoDaVelha.matriz[2, 2] == peca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("Você ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[0, 2] == peca) && (jogoDaVelha.matriz[1, 1] == peca) && (jogoDaVelha.matriz[2, 0] == peca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("Você ganhou!!!!");
+                            break;
+                        }
+
+
+                        Console.WriteLine();
+                        jogoDaVelha.InsereDadoRandom(outraPeca);
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.WriteLine();
+                            for (int j = 0; j < 3; j++)
+                            {
+                                Console.Write($"{jogoDaVelha.matriz[i, j]}  ");
+                            }
+                        }
+
+
+                        //PC - HORIZONTAL
+                        if ((jogoDaVelha.matriz[0, 0] == outraPeca) && (jogoDaVelha.matriz[0, 1] == outraPeca) && (jogoDaVelha.matriz[0, 2] == outraPeca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("O PC ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[1, 0] == outraPeca) && (jogoDaVelha.matriz[1, 1] == outraPeca) && (jogoDaVelha.matriz[1, 2] == outraPeca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("O PC ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[2, 0] == outraPeca) && (jogoDaVelha.matriz[2, 1] == outraPeca) && (jogoDaVelha.matriz[2, 2] == outraPeca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("O PC ganhou!!!!");
+                            break;
+                        }
+
+                        //PC - VERTICAL
+                        if ((jogoDaVelha.matriz[0, 0] == outraPeca) && (jogoDaVelha.matriz[1, 0] == outraPeca) && (jogoDaVelha.matriz[2, 0] == outraPeca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("O PC ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[0, 1] == outraPeca) && (jogoDaVelha.matriz[1, 1] == outraPeca) && (jogoDaVelha.matriz[2, 1] == outraPeca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("O PC ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[0, 2] == outraPeca) && (jogoDaVelha.matriz[1, 2] == outraPeca) && (jogoDaVelha.matriz[2, 2] == outraPeca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("O PC ganhou!!!!");
+                            break;
+                        }
+
+                        //PC - DIAGONAL
+                        if ((jogoDaVelha.matriz[0, 0] == outraPeca) && (jogoDaVelha.matriz[1, 1] == outraPeca) && (jogoDaVelha.matriz[2, 2] == outraPeca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("O PC ganhou!!!!");
+                            break;
+                        }
+                        else if ((jogoDaVelha.matriz[0, 2] == outraPeca) && (jogoDaVelha.matriz[1, 1] == outraPeca) && (jogoDaVelha.matriz[2, 0] == outraPeca))
+                        {
+                            ganhou++;
+                            Console.WriteLine("O PC ganhou!!!!");
+                            break;
+                        }
+                        cont++;
+                        if (cont == 5)
+                        {
+                            Console.WriteLine("Empate");
+                            ganhou++;
+                        }
+                    } while (ganhou == 1);
+                    break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.GetType().FullName);
+                    Console.WriteLine("Informe apenas numeros.");
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.GetType().FullName);
+                    Console.WriteLine("O numero informado foi muito grande.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.GetType().FullName);
+                    Console.WriteLine("Erro desconhecido contate o programdor!");
+                }
+
+            } while (true);
+        }
+    }
+}
